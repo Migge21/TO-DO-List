3d415b39b07d17f0803c5d6b6602092d1a77c203
Migge21 <22001964@aluno.cotemig.com.br>
2023-12-01 22:51:30 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace TO_DO_List
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.txtTarefa = new System.Windows.Forms.RichTextBox();
+            this.cbxPrioridade = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtgTarefa = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgTarefa)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(505, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Insira o que deseja fazer:";
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Location = new System.Drawing.Point(528, 206);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(144, 44);
+            this.btnAdicionar.TabIndex = 1;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // txtTarefa
+            // 
+            this.txtTarefa.Location = new System.Drawing.Point(193, 56);
+            this.txtTarefa.Name = "txtTarefa";
+            this.txtTarefa.Size = new System.Drawing.Size(785, 81);
+            this.txtTarefa.TabIndex = 3;
+            this.txtTarefa.Text = "";
+            // 
+            // cbxPrioridade
+            // 
+            this.cbxPrioridade.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxPrioridade.FormattingEnabled = true;
+            this.cbxPrioridade.Items.AddRange(new object[] {
+            "Urgente",
+            "Alta",
+            "Média",
+            "Baixa"});
+            this.cbxPrioridade.Location = new System.Drawing.Point(193, 167);
+            this.cbxPrioridade.Name = "cbxPrioridade";
+            this.cbxPrioridade.Size = new System.Drawing.Size(785, 24);
+            this.cbxPrioridade.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(558, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 22);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Prioridade:";
+            // 
+            // dtgTarefa
+            // 
+            this.dtgTarefa.AllowUserToAddRows = false;
+            this.dtgTarefa.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dtgTarefa.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dtgTarefa.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dtgTarefa.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dtgTarefa.Location = new System.Drawing.Point(30, 299);
+            this.dtgTarefa.Name = "dtgTarefa";
+            this.dtgTarefa.RowHeadersVisible = false;
+            this.dtgTarefa.RowHeadersWidth = 51;
+            this.dtgTarefa.RowTemplate.Height = 24;
+            this.dtgTarefa.Size = new System.Drawing.Size(1108, 242);
+            this.dtgTarefa.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1170, 563);
+            this.Controls.Add(this.dtgTarefa);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbxPrioridade);
+            this.Controls.Add(this.txtTarefa);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgTarefa)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.RichTextBox txtTarefa;
+        private System.Windows.Forms.ComboBox cbxPrioridade;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dtgTarefa;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TO_DO_List
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+      
+        DataTable dt = new DataTable();
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            
+            dt.Rows.Add(txtTarefa.Text,cbxPrioridade.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dt.Columns.Add("Tarefa", typeof(string));
+            dt.Columns.Add("Prioridade", typeof(string));
+
+            dtgTarefa.DataSource = dt;
+         
+            DataGridViewButtonColumn button = new DataGridViewButtonColumn();
+            {
+                button.Name = "button";
+                button.HeaderText = "Button";
+                button.Text = "Button";
+                button.UseColumnTextForButtonValue = true; //dont forget this line
+                dtgTarefa.Columns.Add(button);
+            }
+
+        }
+    }
+}
